6211f9a8542427d66f391e67b2baa555e1460ed7
rafael <rafael_sousa2018@outlook.com>
2024-04-03 19:11:54 -0300

    remove dist folder

--- build/src/db.d.ts
+++
@@ -1,3 +0,0 @@
-import { Client } from 'pg';
-declare const client: Client;
-export default client;
--- build/src/env.d.ts
+++
@@ -1,8 +0,0 @@
-declare const env: {
-    postgresIntegrationHostPrd: string;
-    postgresIntegrationPortPrd: number;
-    postgresIntegrationUserPrd: string;
-    postgresIntegrationPwdPrd: string;
-    postgresIntegrationDataBasePrd: string;
-};
-export default env;
